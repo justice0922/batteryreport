9ef00f0a0b7a516cc6f412e09e7cf31441bbf454
Jussie.ho <jussie.ho@ude-corp.com>
2021-12-18 01:20:09 +0800

    chore: add python file

---
+++ python/batteryreportScript.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 00:27:24 2021
+
+@author: jussie.ho
+"""
+
+
+import os;
+
+cmdStr = "powercfg/batteryreport";
+os.system(cmdStr);
+os.system("battery-report.html");
